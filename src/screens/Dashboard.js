f59d140fda1ac983c03fc643e03a7839b51c74d1
Alex Dunne <hi@alexdunne.net>
2018-03-10 10:01:25 +0000

    Included the google-map-react library and rendered a map

--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { Component } from "react";
+import GoogleMapReact from "google-map-react";
 
-const Dashboard = () => {
-  return <div>Dashboard</div>;
-};
+// Taken from https://www.fullstackreact.com/articles/how-to-write-a-google-maps-react-component/
+const GOOGLE_MAPS_API_KEY = "AIzaSyAyesbQMyKVVbBgKVi2g6VX7mop2z96jBo";
+
+class Dashboard extends Component {
+  static defaultProps = {
+    center: { lat: 52.488656, lng: -1.887221 },
+    zoom: 15
+  };
+
+  render() {
+    return (
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: [GOOGLE_MAPS_API_KEY] }}
+        defaultCenter={this.props.center}
+        defaultZoom={this.props.zoom}
+      />
+    );
+  }
+}
 
 export default Dashboard;
